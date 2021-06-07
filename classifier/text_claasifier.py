a880746464a1fb3fc04504d318acaead3e2eaf82
Hail-cali <willnotproud5@gmail.com>
2021-06-07 13:04:00 +0900

    dev classifier

--- classifier/text_claasifier.py
+++ classifier/text_claasifier.py
@@ -1,6 +1,14 @@
 import numpy as np
 from collections import defaultdict
 
+class BaseClassifer:
+    def __init__(self):
+        pass
+    def fit(self):
+        pass
+    def transform(self):
+        pass
+
 class TextClassifier(object):
 
     def __init__(self):
@@ -17,24 +25,3 @@ class TextClassifier(object):
 
     def importent_word_(self):
         pass
-
-class TextClusutering(object):
-
-    def __init__(self, k=5):
-        self.k = k
-        self.doc2vec = defaultdict()
-        self.adj = []
-        self.vectorizer = False
-        self.X = False
-
-    def __call__(self):
-        pass
-
-    def process(self, corpus):
-
-        return corpus
-
-    def make_matrix(self, *args):
-
-
-        return
